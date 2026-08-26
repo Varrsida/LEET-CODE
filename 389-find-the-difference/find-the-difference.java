a5f40f1c7b759f6d0f72d4e28b72583ec3b5bd9a
Varrsida <varrsidabommi@gmail.com>
2026-08-26 20:37:00 +0530

    Time: 2 ms (66.28%) | Memory: 42.9 MB (93.20%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.java
@@ -0,0 +1,14 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+
+        int sum = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            sum += t.charAt(i) - s.charAt(i);
+        }
+
+        sum += t.charAt(t.length() - 1);
+
+        return (char) sum;
+    }
+}
